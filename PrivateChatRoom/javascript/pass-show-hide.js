7eeac0ed18bebe24b7cb153e7b618d7bc158978a
Themis Papameletiou <122690021+NotOpti@users.noreply.github.com>
2024-05-14 12:54:18 +0300

    Update pass-show-hide.js
    added comments

--- PrivateChatRoom/javascript/pass-show-hide.js
+++ PrivateChatRoom/javascript/pass-show-hide.js
@@ -1,7 +1,10 @@
+// Selecting the password field and the toggle icon
 const pswrdField = document.querySelector(".form input[type='password']"),
 toggleIcon = document.querySelector(".form .field i");
 
+// Event listener for the toggle icon click
 toggleIcon.onclick = () =>{
+  // Toggles the visibility of the password field and the appearance of the toggle icon
   if(pswrdField.type === "password"){
     pswrdField.type = "text";
     toggleIcon.classList.add("active");
@@ -10,3 +13,5 @@ toggleIcon.onclick = () =>{
     toggleIcon.classList.remove("active");
   }
 }
+
+// This code enables toggling between the password field being visible or hidden.
