f9a2276976e2f45bea8a2a4be3fcfc6e8945531e
Themis Papameletiou <122690021+NotOpti@users.noreply.github.com>
2024-05-14 12:55:12 +0300

    Update config.php
    added comments

--- PrivateChatRoom/php/config.php
+++ PrivateChatRoom/php/config.php
@@ -1,11 +1,15 @@
 <?php
+// Database connection credentials
   $hostname = "localhost";
   $username = "root";
   $password = "";
   $dbname = "chatapp";
 
+  // Establishing a connection to the database
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
+
+  // Checking if the connection was successful
   if(!$conn){
-    echo "Database connection error".mysqli_connect_error();
+    echo "Database connection error".mysqli_connect_error(); // Output error message if connection fails
   }
 ?>
